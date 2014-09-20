6aa15ec0d119e1436ff9ff96279a24163b0ac825
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/ImproveGadgetsDefinition.js
+++ src/ImproveGadgetsDefinition.js
@@ -2,7 +2,6 @@
  * Creates links to the JS and CSS pages of each gadget defined at [[MediaWiki:Gadgets-definition]]
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/ImproveGadgetsDefinition.js]] ([[File:User:Helder.wiki/Tools/ImproveGadgetsDefinition.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
