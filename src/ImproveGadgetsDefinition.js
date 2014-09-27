2fe98157c7c71c7385fb6042d57b2e5ed3518d79
Helder <he7d3r@gmail.com>
2014-09-27 20:49:00 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/ImproveGadgetsDefinition.js
+++ src/ImproveGadgetsDefinition.js
@@ -4,29 +4,29 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function improveGadgetsDefinition( $content ){
-	var $gadgets = $content.find( 'li' ),
-		linker = function( match, page ){
-			var pg = 'MediaWiki:Gadget-' + $.trim( page );
-			return '|<a href="' + mw.util.getUrl( pg ) + '" title="' + pg + '">' + page + '</a>';
-		};
-	$gadgets.each( function(){
-		var $this = $(this),
-			text = $this.text(),
-			reDefinition = /^\s*([a-zA-Z](?:[-_:.\w\d ]*[a-zA-Z0-9])?)(\s*\[.*?\])?\s*((\|[^|]*)+)\s*$/;
-		if ( ! reDefinition.test( text ) ){
-			return true;
-		}
-		$this.html( text.replace( /\|([^|]+?\.(?:js|css)(?=\||$))/g, linker ) );
-	} );
-}
+	function improveGadgetsDefinition( $content ) {
+		var $gadgets = $content.find( 'li' ),
+			linker = function ( match, page ) {
+				var pg = 'MediaWiki:Gadget-' + $.trim( page );
+				return '|<a href="' + mw.util.getUrl( pg ) + '" title="' + pg + '">' + page + '</a>';
+			};
+		$gadgets.each( function () {
+			var $this = $(this),
+				text = $this.text(),
+				reDefinition = /^\s*([a-zA-Z](?:[-_:.\w\d ]*[a-zA-Z0-9])?)(\s*\[.*?\])?\s*((\|[^|]*)+)\s*$/;
+			if ( !reDefinition.test( text ) ) {
+				return true;
+			}
+			$this.html( text.replace( /\|([^|]+?\.(?:js|css)(?=\||$))/g, linker ) );
+		} );
+	}
 
-if( mw.config.get( 'wgNamespaceNumber' ) === 8 && mw.config.get( 'wgTitle' ) === 'Gadgets-definition' ){
-	mw.loader.using( 'mediawiki.util', function(){
-		mw.hook( 'wikipage.content' ).add( improveGadgetsDefinition );
-	} );
-}
+	if ( mw.config.get( 'wgNamespaceNumber' ) === 8 && mw.config.get( 'wgTitle' ) === 'Gadgets-definition' ) {
+		mw.loader.using( 'mediawiki.util', function () {
+			mw.hook( 'wikipage.content' ).add( improveGadgetsDefinition );
+		} );
+	}
 
 }( mediaWiki, jQuery ) );
